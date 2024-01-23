b091575576cc112669fd34bcc9ece38778260a58
Blockchain Enthusiast <zakriya.mmd5@gmail.com>
2024-01-24 01:23:45 +0500

    Done

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -6,11 +6,10 @@ import Cards2 from './Cards2';
 const ProductDetails = () => {
   const params = useParams();
 
-  // Filter the product based on the ID from the URL params
   const productDetails = data.find((product) => product.id === parseInt(params.id, 10));
 
   if (!productDetails) {
-    // Handle the case where the product is not found
+   
     return <div>Product not found</div>;
   }
 
@@ -18,7 +17,7 @@ const ProductDetails = () => {
     <div>
       <div>Product Id: {params.id}</div>
 
-      {/* Pass the productDetails to the Cards2 component */}
+
       <Cards2
         title={productDetails.title}
         image={productDetails.image}
